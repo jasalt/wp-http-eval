79643b3c0edb022eb17b1aa31c7e2d3b4b404173
Jarkko Saltiola <jarkko@saltiolacode.fi>
2025-08-12 23:16:29 +0300

    move dashboard widget cleanup to development container mu-plugin

--- admin-widget.php
+++ admin-widget.php
@@ -51,28 +51,3 @@ function render_eval_widget() {
     </div>
     <?php
 }
-
-
-// Hide all default dashboard widgets
-function hide_default_dashboard_widgets() {
-    // Remove default widgets
-    remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
-    remove_meta_box('dashboard_activity', 'dashboard', 'normal');
-    remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
-    remove_meta_box('dashboard_primary', 'dashboard', 'side');
-    remove_meta_box('dashboard_secondary', 'dashboard', 'side');
-    remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
-    remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
-    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');
-    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
-	remove_meta_box('dashboard_site_health', 'dashboard', 'normal'); // Site Health Status
-}
-
-add_action('wp_dashboard_setup', 'hide_default_dashboard_widgets');
-
-// Hide welcome panel
-function hide_welcome_panel() {
-    remove_action('welcome_panel', 'wp_welcome_panel');
-}
-
-add_action('wp_dashboard_setup', 'hide_welcome_panel');
--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -83,6 +83,37 @@ else
   echo "define('WP_HTTP_EVAL_API', true);" >> /var/www/html/wp-config.php
   echo "define('WP_HTTP_EVAL_WIDGET', true);" >> /var/www/html/wp-config.php
 
+  mkdir /var/www/html/wp-content/mu-plugins/
+
+cat > /var/www/html/wp-content/mu-plugins/dashboard-cleanup.php << 'EOF'
+<?php
+// Hide all default dashboard widgets
+function hide_default_dashboard_widgets() {
+  // Remove default widgets
+  remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
+  remove_meta_box('dashboard_activity', 'dashboard', 'normal');
+  remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+  remove_meta_box('dashboard_primary', 'dashboard', 'side');
+  remove_meta_box('dashboard_secondary', 'dashboard', 'side');
+  remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
+  remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
+  remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');
+  remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+	remove_meta_box('dashboard_site_health', 'dashboard', 'normal'); // Site Health Status
+}
+
+add_action('wp_dashboard_setup', 'hide_default_dashboard_widgets');
+
+// Hide welcome panel
+function hide_welcome_panel() {
+    remove_action('welcome_panel', 'wp_welcome_panel');
+}
+
+add_action('wp_dashboard_setup', 'hide_welcome_panel');
+?>
+EOF
+
+
 	date > /COMPOSE_INITIALIZED
 
 	echo ""
